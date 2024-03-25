f0eb65b7d5c52438676b0f569867a9381401ea6e
khamidullox <hamidullo22022@gmail.com>
2024-03-26 02:12:50 +0500

    clear add

--- app.js
+++ app.js
@@ -2,6 +2,33 @@ import data from "./data.js";
 
 let template = document.querySelector(".job__template");
 let ulGlobalList = document.querySelector(".job__list");
+
+let headerDiv = document.querySelector(".div");
+let headerList = document.querySelector(".job__link__list");
+let allClear = document.querySelector(".job__clear");
+
+function headerBtn(a) {
+  headerDiv.classList.add("job__link__lang");
+  let headerItem = document.createElement("button");
+  headerItem.classList.add("job__lang__header");
+  headerItem.textContent = a;
+  headerList.appendChild(headerItem);
+  let deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "✖";
+  deleteBtn.classList.add("job__btn__delete");
+  headerItem.appendChild(deleteBtn);
+  allClear.textContent = "Clear";
+  allClear.addEventListener("click", () => {
+    headerItem.remove();
+    headerDiv.setAttribute("class", "div");
+  });
+  deleteBtn.addEventListener("click", () => {
+    headerItem.remove();
+    if (headerList.children.length == 0) {
+      headerDiv.setAttribute("class", "div");
+    }
+  });
+}
 data.forEach((item) => {
   let li = template.content.cloneNode(true);
 
@@ -40,24 +67,41 @@ data.forEach((item) => {
   jobRole.textContent = role;
   jobLvel.textContent = level;
 
+  jobRole.addEventListener("click", () => {
+    headerBtn(role);
+  });
+
+  jobLvel.addEventListener("click", () => {
+    headerBtn(level);
+  });
+
   languages.forEach((lang) => {
     let newLangJob = document.createElement("button");
     newLangJob.classList.add("job__lang");
     newLangJob.textContent = lang;
     jobLanguages.appendChild(newLangJob);
+    newLangJob.addEventListener("click", () => {
+      headerBtn(lang);
+    });
   });
+
   tools.forEach((tool) => {
     let newToolJob = document.createElement("button");
     newToolJob.classList.add("job__tol");
     newToolJob.textContent = tool;
     jobTool.appendChild(newToolJob);
+    newToolJob.addEventListener("click", () => {
+      headerBtn(tool);
+    });
   });
+
   if (item.new) {
     let newSpan = document.createElement("span");
     newSpan.classList.add("job__new");
     newSpan.textContent = "NEW!";
     compan.appendChild(newSpan);
   }
+
   if (featured) {
     let newSpan = document.createElement("span");
     newSpan.classList.add("job__featured");
